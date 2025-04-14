84a09028a199998656e7c92d79d49f111bd552a2
MasnyPen <124733262+MasnyPen@users.noreply.github.com>
2025-04-14 13:08:30 +0200

    cjs exports fix and mongo & pg driver delete

--- src/exports/cjs/index.ts
+++ src/exports/cjs/index.ts
@@ -1,17 +1,14 @@
 module.exports = {
-    DripcordFramework: require('./src/DripcordFramework.js'),
+    DripcordFramework: require('../../DripcordFramework.js'),
     Bot: require('../../BotClient.js'),
-    Event: require('./handlers/Event/Event.js'),
-    Command: require('./handlers/Commands/Command.js'),
-    CacheDriver: require('./drivers/cache/CacheDriver.js'),
-    LocalDriver: require('./drivers/cache/LocalDriver.js'),
-    RedisDriver: require('./drivers/cache/RedisDriver.js'),
+    Event: require('../../handlers/Event/Event.js'),
+    Command: require('../../handlers/Commands/Command.js'),
+    CacheDriver: require('../../drivers/cache/CacheDriver.js'),
+    LocalDriver: require('../../drivers/cache/LocalDriver.js'),
+    RedisDriver: require('../../drivers/cache/RedisDriver.js'),
     discord: require('discord.js'),
-    mongoose: require('mongoose'),
     DatabaseDriver: require('../../drivers/database/DatabaseDriver.js'),
-    PgDatabaseDriver: require('../../drivers/database/PgDriver.js'),
     MySQLDatabaseDriver: require('../../drivers/database/MysqlDriver.js'),
-    MongoDatabaseDriver: require('../../drivers/database/MongoDriver.js'),
     SQLiteDatabaseDriver: require('../../drivers/database/SqliteDriver.js')
   };
   
--- src/exports/esm/index.ts
+++ src/exports/esm/index.ts
@@ -7,13 +7,7 @@ export { LocalCacheDriver } from "../../drivers/cache/LocalDriver.js"
 export { RedisCacheDriver } from "../../drivers/cache/RedisDriver.js"
 
 export { DatabaseDriver } from '../../drivers/database/DatabaseDriver.js'
-export { MongoDatabaseDriver } from '../../drivers/database/MongoDriver.js'
 export { MySQLDatabaseDriver } from '../../drivers/database/MysqlDriver.js'
-export { PgDatabaseDriver } from '../../drivers/database/PgDriver.js'
 export { SQLiteDatabaseDriver } from '../../drivers/database/SqliteDriver.js'
 
 export * from 'discord.js'
-
-export { default as mongoose } from "mongoose";
-export * as pg from "pg";
-export * as mysql from "mysql2";
