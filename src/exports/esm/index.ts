d93fb9464e1651fd0133fe16707bede5042cea29
MasnyPen <124733262+MasnyPen@users.noreply.github.com>
2025-04-26 19:24:59 +0200

    discord.js update and esm export fix

--- src/exports/esm/index.ts
+++ src/exports/esm/index.ts
@@ -9,6 +9,10 @@ export { i18n, Translate } from '../../utils/i18n.js'
 
 export * from 'discord.js';
 
+export { CacheDriver } from "../../drivers/cache/CacheDriver.js"
+export { LocalCacheDriver } from "../../drivers/cache/LocalDriver.js"
+export { RedisCacheDriver } from "../../drivers/cache/RedisDriver.js"
+
 export { DatabaseDriver } from '../../drivers/database/DatabaseDriver.js';
 export { MySQLDatabaseDriver } from '../../drivers/database/MysqlDriver.js';
 export { SQLiteDatabaseDriver } from '../../drivers/database/SqliteDriver.js';
